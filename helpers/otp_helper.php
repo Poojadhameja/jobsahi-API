07c8c7f1465ec3dc40cec4411373fc8e2d127184
bhumikajain54 <jainbhumika54@gmail.com>
2025-08-27 13:22:50 +0530

    Updated Api

--- helpers/otp_helper.php
+++ helpers/otp_helper.php
@@ -6,8 +6,10 @@
  * @param int $length - Length of OTP (default 6)
  * @return string - Generated OTP
  */
-function generateOTP($length = 6) {
-    return str_pad(random_int(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT);
+if (!function_exists('generateOTP')) {
+    function generateOTP($length = 6) {
+        return str_pad(random_int(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT);
+    }
 }
 
 /**
