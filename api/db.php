d2f1066dd0f424c26c601feb6fe74cc1ee032566
AartiNathani <144878986+AartiNathani@users.noreply.github.com>
2025-10-30 00:46:07 +0530

    merge code for simran to aarti

--- api/db.php
+++ api/db.php
@@ -4,7 +4,7 @@ $BASE_DIR = dirname(__DIR__);
 
 // Database Configuration
 $dbHost = '127.0.0.1';
-$dbPort = '3307';
+$dbPort = '3306';
 $dbName = 'jobsahi_database';
 $dbUser = 'root';
 $dbPass = '';
